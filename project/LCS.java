d8b126d5cc7eafc04efdffd99d9fe2b87d4f0a34
agreaves <alexg343@stanford.edu>
2014-05-31 17:19:08 -0700

    Update LCS.java

--- project/LCS.java
+++ project/LCS.java
@@ -7,7 +7,9 @@ public class LCS {
 
 
   static void printLCStable(int[][] arr) {
-    System.out.println(Arrays.toString(arr));  
+    for(int i = 0; i < arr.length; i++) {
+      System.out.println(Arrays.toString(arr[1]));  
+    }
   }
   
   static int LCS() {

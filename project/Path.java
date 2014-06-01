5c6ca3ddb703a182c3f4799afe3d9606ee6e0172
sethjhs <sethjhs@stanford.edu>
2014-05-31 22:53:27 -0700

    path ect

--- project/Path.java
+++ project/Path.java
@@ -1,8 +1,17 @@
 
-public class Path {
 
-	public Path() {
-		// TODO Auto-generated constructor stub
+public class Path {
+	public int startIndex;
+	public BoundElement[] boundry;
+	
+	public Path(int m) {
+		boundry = new BoundElement[m];
+		for(int i =0;i<m;i++){
+			boundry[i].lowerBoundValue=0;
+			boundry[i].upperBoundValue=0;
+			startIndex=0;
+		}
+		
 	}
 
 }
